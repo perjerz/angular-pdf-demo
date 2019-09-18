55be4ea086203ccb6adad1d189a7fe1e1ca190dd
perjerz3434 <perjerz3434@gmail.com>
2019-09-18 16:39:33 +0700

    add pdf demo preview

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import hexToBinary from 'hex-to-binary';
+import { saveAs } from 'file-saver';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'checkmate';
+export class AppComponent implements OnInit {
+  url = 'http://183.90.168.77/api/newgffi/reportfidata';
+  date = new Date();
+  title = '';
+  objectUrl: Blob;
+  constructor(
+    private http: HttpClient) { }
+
+
+  ngOnInit() {
+    const param = {
+      COMP_CODE: "12005",
+      ACC_DOC_NO: "0100000003",
+      FISC_YEAR: "2562",
+      FORMID: "J00"
+    };
+
+    return this.http.post(this.url, param, {responseType: 'text' as 'text'}).subscribe(value => {
+      const text = value.split('<Data>')[1];
+      const base64 = text.substring(0, text.indexOf('<'));
+      const blob = this.base64ToPdfBlob(base64);
+      this.objectUrl = blob;
+    } , error => {
+      console.log(error);
+    });
+  }
+
+  base64ToPdfBlob(base64: string) {
+    const byteArray = new Uint8Array(base64.length / 2);
+    for (let x = 0; x < byteArray.length; x++) {
+        byteArray[x] = parseInt(base64.substr(x * 2, 2), 16);
+    }
+    return new Blob([byteArray], {type: "application/pdf"});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    NgxExtendedPdfViewerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
